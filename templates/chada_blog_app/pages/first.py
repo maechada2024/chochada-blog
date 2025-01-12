a57ef27f5318e0dfc7dd0bd1430fb02ac74b896d
chada2516 <dr.chadchadaporn@gmail.com>
2025-01-12 15:42:58 +0700

    first

---
+++ templates/chada_blog_app/pages/first.py
@@ -0,0 +1,9 @@
+class Cat():
+    
+    def __init__(self, bread, name):
+        self.bread = bread
+        self.name = name
+        
+myCat = Cat("Lab", "Sammy")
+print(myCat.bread)
+print(myCat.name)
